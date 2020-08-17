62dd15a81c1132248278fed5ae47266220b6a1e1
Charles-1994 <RajRulezz94@gmail.com>
2020-08-18 00:22:10 +0530

    Added Example program

---
+++ Example.java
@@ -0,0 +1,8 @@
+public class Example{
+    public static void main(final String args[]) {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Welcome"+i);
+        }
+    }
+}
